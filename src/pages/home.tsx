dd1cf15b58b026088233749d874a9de7051249b2
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-18 19:35:42 -0300

    Pokedex

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,23 @@
 import Cards from "../componetes/cards/Cards";
 import NavBar from "../componetes/navbar/NavBar";
 import "./home.css";
+import pokemonList from "../assets/pokemon.json"
 
 function Home() {
+  // estou dizendo que só quero a propiedade "pokemons"
+  const {pokemons} = pokemonList;
+
   return (
     <>
       <NavBar />
       <h1 className="title">Encontre todos os pokémons em um só lugar</h1>
 
       <div className="list">
-      
-      <Cards id={15} name="burra"/>   
+        {pokemons.slice(1,10).map((pokemon, index) => {
+          // a key é o proprio react q requer algum indentificador unico para cada Card
+        return  <Cards key={index} id={pokemon.id} name={pokemon.name}/> ;
+        })}
+    
         
          
       </div>
