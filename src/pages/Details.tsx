921027a54da3dab4d6ac641afd5148b84ef4ee9f
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-18 21:58:46 -0300

    Pokedex

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -3,13 +3,20 @@ import {useParams} from "react-router-dom"
 import pokemonList from "../assets/pokemon.json"
 
 function Details(){
+    // ela vai pegar o id da url
     const {id} = useParams();
+
+    // ela vai pegar o pokemon do banco de dados
     const {pokemons}= pokemonList;
+
 
     return (
         <>
             <NavBar/>
+            {/* se o id da url for igual o id do banco de dados ? faça isso */}
             <h1>{pokemons.find((pokemon)=> String(pokemon.id) == id)?.name}</h1>
+
+            
         </>
         )
     }
