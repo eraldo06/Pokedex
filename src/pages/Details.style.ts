0e1d6f058bc61ecaba4c938ba56833e56c9396f8
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-23 17:17:51 -0300

    pokedex

---
+++ src/pages/Details.style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: calc(100vh - 4rem 40px);
+`;
+
+
+export const Imagem = styled.img`
+
+`;
+
+
+export const Card = styled.div`
+    background-color: #8bbe8a;
+    border: 4px solid #62b957;
+    box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+    border-radius: 20px;
+    padding: 50px;
+`;
+
+export const Number = styled.span`
+
+`;
+
+
+export const Title = styled.h1`
+
+`;
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,20 +1,61 @@
-import NavBar from "../componetes/navbar/NavBar";
+import {useState, useEffect} from "react"
 import {useParams} from "react-router-dom"
-import pokemonList from "../assets/pokemon.json"
+
+import NavBar from "../componetes/navbar/NavBar";
+import Badge from "../componetes/badge/Badge";
+import { CardPokemonProps } from "../componetes/cards/Cards";
+import api from "../services/api";
+
+import {Container, Imagem, Card, Number, Title} from "./Details.style"
+
 
 function Details(){
     // ela vai pegar o id da url
     const {id} = useParams();
+    const 
+    const [pokemonData, setPokemonData] = useState<CardPokemonProps>(
+        {} as CardPokemonProps
+    );
 
-    // ela vai pegar o pokemon do banco de dados
-    const {pokemons}= pokemonList;
+    async function GetPokemonData() {
+        const { data } = await api.get("pokemon/" + id);
+
+        setPokemonData ({
+            id: data.id,
+            name: data.name,
+            types: data.types,
+        });
+
+        useEffect(() => {
+            GetPokemonData();
+        },[]);
+    }
+
 
 
     return (
         <>
             <NavBar/>
             {/* se o id da url for igual o id do banco de dados ? faça isso */}
-            <h1>{pokemons.find((pokemon)=> String(pokemon.id) == id)?.name}</h1>
+
+            <Container>
+                <Imagem src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}/>
+                <Card>
+        
+                <Number>#{String(id).toString().padStart(3, "0")}</Number>
+                <Title>{GetPokemonData.name}</Title>
+
+                {pokemonData.types.map((item, index)=>{
+                    return <Badge key={index} name={item.
+                        type.name}/>
+                    
+                })}
+                </Card>
+
+            </Container>
+
+
+
 
             
         </>
